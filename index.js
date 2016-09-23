78400f40606c032fb01542d35e579dc851d82fb3
Federico Maffei <federico.maffei@gmail.com>
2016-09-23 11:56:34 +0100

    Step 1: server and scaffolding done

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+process.on("SIGINT", function() { console.log("Caught SIGINT"); process.exit(0); });
+process.on("SIGTERM", function() { console.log("Caught SIGTERM"); process.exit(0); });
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({
+    port: 3000
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
